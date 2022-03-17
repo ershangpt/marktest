edfbc8980216e2346bc9d7b9b6468c42e739fc3b
shashank <ershangpt@gmail.com>
2022-03-17 15:28:35 +0530

    second commit

--- arrange.js
+++ arrange.js
@@ -1,15 +1,15 @@
 const shuffleBtn = document.getElementById("shuffleBtn");
-  const sortBtn = document.getElementById("sortBtn");
-  let gridContainer = document.getElementById("gridContainer");
+const sortBtn = document.getElementById("sortBtn");
+let gridContainer = document.getElementById("gridContainer");
 
-  function shuffleArray(array) {
+function shuffleArray(array) {
 	for (let i = array.length - 1; i > 0; i--) {
 	  const j = Math.floor(Math.random() * (i + 1));
 	  [array[i], array[j]] = [array[j], array[i]];
 	}
-  }
+}
 
-  const colors = [
+const colors = [
 	"#000000",
 	"#2B8EAD",
 	"#333333",
@@ -19,9 +19,9 @@ const shuffleBtn = document.getElementById("shuffleBtn");
 	"#EFEFEF",
 	"#2F454E",
 	"#72C3DC",
-  ];
+];
 
-  sortBtn.addEventListener("click", () => {
+sortBtn.addEventListener("click", () => {
 	let divs = "";
 	for (let i = 0; i < colors.length; i++) {
 	  divs +=
@@ -36,9 +36,9 @@ const shuffleBtn = document.getElementById("shuffleBtn");
 		"</div></div></div>";
 	}
 	gridContainer.innerHTML = divs;
-  });
+});
 
-  shuffleBtn.addEventListener("click", () => {
+shuffleBtn.addEventListener("click", () => {
 	let copyColors = [...colors];
 	shuffleArray(copyColors);
 	let divs = "";
@@ -55,6 +55,6 @@ const shuffleBtn = document.getElementById("shuffleBtn");
 		"</div></div></div>";
 	}
 	gridContainer.innerHTML = divs;
-  });
+});
 
-  sortBtn.click();
+sortBtn.click();
